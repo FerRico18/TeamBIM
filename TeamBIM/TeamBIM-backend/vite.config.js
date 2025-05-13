f3b40ee2d341930657dbb43d16e317820774f0f8
Fer <ricfernanda9@gmail.com>
2025-05-13 15:55:30 -0600

    configs

--- TeamBIM/TeamBIM-backend/vite.config.js
+++ TeamBIM/TeamBIM-backend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      // Si decides usar rutas tipo `/api/register`, las redirige al backend
+      '/api': 'http://localhost:9001',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
